4095f5d4beb539eb81dc425382be99a103c50a52
C.J.  Black <christopher_black@brown.edu>
2016-11-09 15:36:23 -0500

    more

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,23 +1,23 @@
 
 var path_c = new Path.Circle({
-  center: [40, 50],
-  radius: 32,
+  center: [10, 50],
+  radius: 20,
   fillColor: '#0020FF',
 });
 var path_c1 = new Path.Circle({
-  center: [80, 50],
-  radius: 32,
+  center: [25, 50],
+  radius: 20,
   fillColor: '#0040FF',
 });
 var path_c2 = new Path.Circle({
-  center: [120, 50],
-  radius: 32,
+  center: [40, 50],
+  radius: 20,
     fillColor: '#0060FF',
 
 });
 var path_c3 = new Path.Circle({
-  center: [160, 50],
-  radius: 32,
+  center: [55, 50],
+  radius: 20,
  fillColor: '#0080FF',
 });
 
