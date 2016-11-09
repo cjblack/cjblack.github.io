e80cd81790b8e58fd839b42e9db2368cf5ea0282
C.J.  Black <christopher_black@brown.edu>
2016-11-09 15:01:23 -0500

    editing paper

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,47 +1,30 @@
+
+var path_c = new Path.Circle({
+  center: [40, 50],
+  radius: 32,
+  fillColor: '#0020FF',
+});
 var path_c1 = new Path.Circle({
   center: [80, 50],
-  radius: 35
+  radius: 32,
+  fillColor: '#0040FF',
 });
-// Set the name of the path:
-path_c1.name = 'example';
-
-// Create a group and add path to it as a child:
-var group = new Group();
-group.addChild(path_c1);
-
-// The path can be accessed by name:
-group.children['example'].fillColor = '#0000FF';
-
 var path_c2 = new Path.Circle({
   center: [120, 50],
-  radius: 35
+  radius: 32,
+    fillColor: '#0060FF',
+
 });
-// Set the name of the path:
-path_c2.name = 'example1';
+var path_c3 = new Path.Circle({
+  center: [160, 50],
+  radius: 32,
+ fillColor: '#0080FF',
+});
 
-// Create a group and add path to it as a child:
-var group2 = new Group();
-group2.addChild(path_c2);
-group2.children['example1'].fillColor = '#0099FF';
-group.children['example'].blendMode = 'color-burn';
 
-// var circle1 = new Path.Circle({
-//     center: view.center,
-//     radius: 30
-// });
-//
-// circle1.fillColor = new Color(0, 0, 138);
-// var circle2 = new Path.Circle({
-//     center: [120,50],
-//     radius: 30
-// });
-// circle2.fillColor = new Color(42, 91, 215);
-//
-// circle2.blendMode = 'source-out';
+path_c1.blendMode = 'hue';
 
-// path.on('mouseenter', function() {
-//
-// });
-// path.on('mouseleave', function(){
-//
-// });
+path_c2.blendMode = 'hue';
+
+
+path_c3.blendMode = 'hue';
