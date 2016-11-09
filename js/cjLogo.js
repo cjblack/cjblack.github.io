f8aa00922638e74fc6fad075dda54b9c0dad1dcb
C.J.  Black <christopher_black@brown.edu>
2016-11-08 21:29:38 -0500

    change in logo

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,5 +1,19 @@
-var path = new Path();
-path.strokeColor = 'black';
-var start = new Point(100,100);
-path.moveTo(start);
-path.lineTo(start+[100,-50]);
+var circle1 = new Path.Circle({
+    center: [80,50],
+    radius: 35,
+});
+circle1.fillColor = new Color(0, 0, 138);
+var circle2 = new Path.Circle({
+    center: [120,50],
+    radius: 35,
+});
+circle2.fillColor = new Color(42, 91, 215);
+
+circle2.blendMode = 'source-out';
+
+// path.on('mouseenter', function() {
+//
+// });
+// path.on('mouseleave', function(){
+//
+// });
