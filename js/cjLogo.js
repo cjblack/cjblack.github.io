bd4a73e1945e2df0db66424158531706688118ca
C.J.  Black <christopher_black@brown.edu>
2016-11-09 09:47:07 -0500

    found the problem

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,10 +1,20 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+var circle1 = new Path.Circle({
+    center: view.center,
+    radius: 30
+});
+
+circle1.fillColor = new Color(0, 0, 138);
+var circle2 = new Path.Circle({
+    center: [120,50],
+    radius: 30
+});
+circle2.fillColor = new Color(42, 91, 215);
+
+circle2.blendMode = 'source-out';
+
+// path.on('mouseenter', function() {
+//
+// });
+// path.on('mouseleave', function(){
+//
+// });
