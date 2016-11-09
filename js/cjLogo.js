6355b8e55b081bd45f28c06eb8d4471d5da108d6
C.J.  Black <christopher_black@brown.edu>
2016-11-09 09:41:53 -0500

    test

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,24 +1,4 @@
-var circle1 = new Path.Circle({
-    center: view.center,
-    radius: 10,
-    strokeColor: 'blue',
-});
-
-function onResize(event){
-  path.position = view.center;
-}
-// circle1.fillColor = new Color(0, 0, 138);
-// var circle2 = new Path.Circle({
-//     center: [120,50],
-//     radius: 35
-// });
-// circle2.fillColor = new Color(42, 91, 215);
-//
-// circle2.blendMode = 'source-out';
-
-// path.on('mouseenter', function() {
-//
-// });
-// path.on('mouseleave', function(){
-//
-// });
+var myPath = new Path();
+myPath.strokeColor = 'black';
+myPath.add(new Point(0, 0));
+myPath.add(new Point(100, 50));
