ac0e5489bd789fa23043578710cb8544b2af377c
C.J.  Black <christopher_black@brown.edu>
2016-11-09 11:36:37 -0500

    paperscript working

--- js/cjLogo.js
+++ js/cjLogo.js
@@ -1,16 +1,31 @@
-var circle1 = new Path.Circle({
-    center: view.center,
-    radius: 30
+var path = new Path.Circle({
+  center: [80, 50],
+  radius: 35
 });
+// Set the name of the path:
+path.name = 'example';
 
-circle1.fillColor = new Color(0, 0, 138);
-var circle2 = new Path.Circle({
-    center: [120,50],
-    radius: 30
-});
-circle2.fillColor = new Color(42, 91, 215);
+// Create a group and add path to it as a child:
+var group = new Group();
+group.addChild(path);
 
-circle2.blendMode = 'source-out';
+// The path can be accessed by name:
+group.children['example'].fillColor = 'blue';
+
+
+// var circle1 = new Path.Circle({
+//     center: view.center,
+//     radius: 30
+// });
+//
+// circle1.fillColor = new Color(0, 0, 138);
+// var circle2 = new Path.Circle({
+//     center: [120,50],
+//     radius: 30
+// });
+// circle2.fillColor = new Color(42, 91, 215);
+//
+// circle2.blendMode = 'source-out';
 
 // path.on('mouseenter', function() {
 //
